03dca14f8613958be3a1e932a7883c888ff76c4b
zeroviral <kshari7@msn.com>
2020-11-23 16:01:06 -0500

    Adding config file, and overhauling for OOP and cleanliness within main p5 bot (digital)

--- p5_digital_walmart_bot.py
+++ p5_digital_walmart_bot.py
@@ -1,100 +1,118 @@
 from selenium import webdriver
+from configparser import ConfigParser
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
-def order():
-    # VARIABLES
-    addToCart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
-    checkOut = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]'
-    continueWithoutAccount = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span'
-    firstContinue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span'
-    firstName ='//*[@id="firstName"]'
-    lastName = '//*[@id="lastName"]'
-    email = '//*[@id="email"]'
-    address = '//*[@id="addressLineOne"]'
-    phone = '//*[@id="phone"]'
-    confirmInfo = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span'
-    creditCardNum = '//*[@id="creditCard"]'
-    creditExpireMonth = '//*[@id="month-chooser"]'
-    creditExpireYear = '//*[@id="year-chooser"]'
-    creditCVV = '//*[@id="cvv"]'
-    reviewOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span'
-    confirmOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
-    notAvailableMessage = '//div[@class="prod-blitz-copy-message"]'
+class P5Bot():
 
-    # KEYS
-    # Add your information here
-    myFirstName = 'John'
-    myLastName = 'Smith'
-    myEmail = 'mail@gmail.com'
-    myAddress = '1234 Apple Lane'
-    myPhone = '1234567890'
-    myCreditCardNum = '123456789'
-    myCreditExpireMonth = '00'
-    myCreditExpireYear = '25'
-    myCVV = '123'
+    def __init__(self):
 
-    if not verify_if_available(notAvailableMessage):
-        driver.quit()
-        return
+        # VARIABLES
+        self.add_to_cart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
+        self.check_out = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]'
+        self.continue_without_account = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span'
+        self.first_continue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span'
+        self.first_name = '//*[@id="firstName"]'
+        self.last_name = '//*[@id="lastName"]'
+        self.email = '//*[@id="email"]'
+        self.address = '//*[@id="addressLineOne"]'
+        self.phone = '//*[@id="phone"]'
+        self.confirm_info = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span'
+        self.credit_card_num = '//*[@id="creditCard"]'
+        self.credit_expire_month = '//*[@id="month-chooser"]'
+        self.credit_expire_year = '//*[@id="year-chooser"]'
+        self.credit_cvv = '//*[@id="cvv"]'
+        self.review_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[' \
+                        '3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span'
+        self.confirm_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[' \
+                         '3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
+        self.not_available_message = '//div[@class="prod-blitz-copy-message"]'
 
-    # ADDS PS5 TO CART AND GOES TO CHECKOUT
-    click_button(addToCart)
-    click_button(checkOut)
-    click_button(continueWithoutAccount)
+    def order(self):
 
-    # FILLS OUT SHIPPING INFO
-    click_button(firstContinue)
-    enter_data(firstName, myFirstName)
-    enter_data(lastName, myLastName)
-    enter_data(phone, myPhone)
-    enter_data(email, myEmail)
-    enter_data(address, myAddress)
-    click_button(confirmInfo)
+        # KEYS
+        # Add your information here
+        config = ConfigParser()
+        config.read('config.ini')
 
-    # FILLS OUT PAYMENT
-    enter_data(creditCardNum, myCreditCardNum)
-    enter_data(creditExpireMonth, myCreditExpireMonth)
-    enter_data(creditExpireYear, myCreditExpireYear)
-    enter_data(creditCVV, myCVV)
+        first_name = config.get('main', 'firstName')
+        last_name = config.get('main', 'lastName')
+        email = config.get('main', 'email')
+        address = config.get('main', 'address')
+        phone = config.get('main', 'phone')
+        cc_num = config.get('main', 'creditCardNum')
+        cc_expiry_month = config.get('main', 'creditExpireMonth')
+        cc_expiry_year = config.get('main', 'creditExpireYear')
+        cvv = config.get('main', 'cvv')
 
+        if not self.verify_if_available(self.not_available_message):
+            driver.quit()
+            return
 
-    # ORDER
-    click_button(reviewOrder)
-    # clickButton(confirmOrder)
+        # ADDS PS5 TO CART AND GOES TO CHECKOUT
+        self.click_button(self.addToCart)
+        self.click_button(self.checkOut)
+        self.click_button(self.continueWithoutAccount)
 
+        # FILLS OUT SHIPPING INFO
+        self.click_button(self.firstContinue)
+        self.enter_data(self.firstName, first_name)
+        self.enter_data(self.lastName, last_name)
+        self.enter_data(phone, phone)
+        self.enter_data(email, email)
+        self.enter_data(address, address)
+        self.click_button(self.confirmInfo)
 
-def verify_if_available(xpath):
-    if len(driver.find_elements_by_xpath(xpath)):
-        print("Seems like it's not available currently, exiting program")
-        return False
-    else:
-        return True
+        # FILLS OUT PAYMENT
+        self.enter_data(self.creditCardNum, cc_num)
+        self.enter_data(self.creditExpireMonth, cc_expiry_month)
+        self.enter_data(self.creditExpireYear, cc_expiry_year)
+        self.enter_data(self.creditCVV, cvv)
 
+        # ORDER
+        self.click_button(self.reviewOrder)
+        # clickButton(confirmOrder)
 
-def click_button(xpath):
-    try:
-        driver.find_element_by_xpath(xpath).click()
-        pass
-    except Exception:
-        time.sleep(1)
-        click_button(xpath)
+    @staticmethod
+    def verify_if_available(xpath):
+        if len(driver.find_elements_by_xpath(xpath)):
+            print("Seems like it's not available currently, exiting program")
+            return False
+        else:
+            return True
 
+    def click_button(self, xpath):
+        try:
+            driver.find_element_by_xpath(xpath).click()
+            pass
+        except Exception:
+            time.sleep(1)
+            self.click_button(xpath)
 
-def enter_data(field, data):
-    try:
-        driver.find_element_by_xpath(field).send_keys(data)
-        pass
-    except Exception:
-        time.sleep(1)
-        enter_data(field, data)
+    def enter_data(self, field, data):
+        try:
+            driver.find_element_by_xpath(field).send_keys(data)
+        except Exception:
+            time.sleep(1)
+            self.enter_data(field, data)
 
 
 if __name__ == "__main__":
-    # driver = webdriver.Chrome("./chromedriver")
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    # Replace this link with a new value for the link to the version you want
-    driver.get('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
-    time.sleep(3)
-    order()
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        # Replace this link with a new value for the link to the version you want
+
+        # Digital edition
+        # driver.get('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
+
+        # Physical edition
+        driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942')
+        time.sleep(3)
+
+        ps5bot = P5Bot()
+        ps5bot.order()
+    except Exception as e:
+        print("Exception occured: " + str(e))
+        print("\n Quitting chromeDriver...")
+        driver.quit()
