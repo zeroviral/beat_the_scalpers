44b126c3646a34156ae420cb0baee61edd2e188a
zeroviral <kshari7@msn.com>
2020-11-23 17:08:25 -0500

    Updating code to enhance xPath references.

--- p5_digital_walmart_bot.py
+++ p5_digital_walmart_bot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from configparser import ConfigParser
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 
@@ -9,10 +10,10 @@ class P5Bot():
     def __init__(self):
 
         # VARIABLES
-        self.add_to_cart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
-        self.check_out = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]'
+        self.add_to_cart = '//button[@data-tl-id="ProductPrimaryCTA-cta_add_to_cart_button"]'
+        self.check_out = '//span[contains(text(), "Check out")]'
         self.continue_without_account = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span'
-        self.first_continue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span'
+        self.continue_to_delivery = '//button[@aria-label="Continue to Delivery Address"]'
         self.first_name = '//*[@id="firstName"]'
         self.last_name = '//*[@id="lastName"]'
         self.email = '//*[@id="email"]'
@@ -23,10 +24,8 @@ class P5Bot():
         self.credit_expire_month = '//*[@id="month-chooser"]'
         self.credit_expire_year = '//*[@id="year-chooser"]'
         self.credit_cvv = '//*[@id="cvv"]'
-        self.review_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[' \
-                        '3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span'
-        self.confirm_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[' \
-                         '3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
+        self.review_order = '//span[contains(text(), "Review your order")]'
+        self.confirm_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
         self.not_available_message = '//div[@class="prod-blitz-copy-message"]'
 
     def order(self):
@@ -36,6 +35,7 @@ class P5Bot():
         config = ConfigParser()
         config.read('config.ini')
 
+        print_list = []
         first_name = config.get('main', 'firstName')
         last_name = config.get('main', 'lastName')
         email = config.get('main', 'email')
@@ -46,33 +46,45 @@ class P5Bot():
         cc_expiry_year = config.get('main', 'creditExpireYear')
         cvv = config.get('main', 'cvv')
 
+        print_list.append(first_name)
+        print_list.append(last_name)
+        print_list.append(email)
+        print_list.append(address)
+        print_list.append(phone)
+        print_list.append(cc_num)
+        print_list.append(cc_expiry_month)
+        print_list.append(cc_expiry_year)
+        print_list.append(cvv)
+
+        print("Current values: " + str(print_list))
+
         if not self.verify_if_available(self.not_available_message):
             driver.quit()
             return
 
         # ADDS PS5 TO CART AND GOES TO CHECKOUT
-        self.click_button(self.addToCart)
-        self.click_button(self.checkOut)
-        self.click_button(self.continueWithoutAccount)
+        self.click_button(self.add_to_cart)
+        self.click_button(self.check_out)
+        self.click_button(self.continue_without_account)
 
         # FILLS OUT SHIPPING INFO
-        self.click_button(self.firstContinue)
-        self.enter_data(self.firstName, first_name)
-        self.enter_data(self.lastName, last_name)
+        self.click_button(self.continue_to_delivery)
+        self.enter_data(self.first_name, first_name)
+        self.enter_data(self.last_name, last_name)
         self.enter_data(phone, phone)
         self.enter_data(email, email)
         self.enter_data(address, address)
-        self.click_button(self.confirmInfo)
+        self.click_button(self.confirm_info)
 
         # FILLS OUT PAYMENT
-        self.enter_data(self.creditCardNum, cc_num)
-        self.enter_data(self.creditExpireMonth, cc_expiry_month)
-        self.enter_data(self.creditExpireYear, cc_expiry_year)
-        self.enter_data(self.creditCVV, cvv)
+        self.enter_data(self.credit_card_num, cc_num)
+        self.enter_data(self.credit_expire_month, cc_expiry_month)
+        self.enter_data(self.credit_expire_year, cc_expiry_year)
+        self.enter_data(self.credit_cvv, cvv)
 
         # ORDER
         self.click_button(self.reviewOrder)
-        # clickButton(confirmOrder)
+        self.clickButton(self.confirmOrder)
 
     @staticmethod
     def verify_if_available(xpath):
@@ -82,25 +94,32 @@ class P5Bot():
         else:
             return True
 
-    def click_button(self, xpath):
+    @staticmethod
+    def click_button(xpath):
         try:
+            # time.sleep(1.5)
             driver.find_element_by_xpath(xpath).click()
-            pass
-        except Exception:
-            time.sleep(1)
-            self.click_button(xpath)
+        except NoSuchElementException:
+            print("Element not found: " + xpath)
+            print("\nExiting program...")
+            driver.quit()
+            exit(1)
 
     def enter_data(self, field, data):
         try:
             driver.find_element_by_xpath(field).send_keys(data)
-        except Exception:
-            time.sleep(1)
             self.enter_data(field, data)
+        except NoSuchElementException:
+            print("Field not found for text entry: " + field)
+            print("\nExiting Program...")
+            driver.quit()
+            exit(1)
 
 
 if __name__ == "__main__":
     try:
         driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.implicitly_wait(5)
         # Replace this link with a new value for the link to the version you want
 
         # Digital edition
@@ -108,10 +127,14 @@ if __name__ == "__main__":
 
         # Physical edition
         driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942')
+
+        # Test URL
+        # driver.get('https://www.walmart.com/ip/Apple-AirPods-Pro/520468661')
         time.sleep(3)
 
         ps5bot = P5Bot()
         ps5bot.order()
+
     except Exception as e:
         print("Exception occured: " + str(e))
         print("\n Quitting chromeDriver...")
